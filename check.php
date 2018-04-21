f44476e9d8c09b8e47896e2fdbb2700247183a04
Anthony Kuske <anthonykuske@gmail.com>
2018-04-21 18:38:55 +0100

    Initial commit

---
+++ check.php
@@ -0,0 +1,53 @@
+<?php
+
+$config = require __DIR__.'/config.php';
+
+function testConnection(string $host, int $port)
+{
+    $date = new DateTime('NOW', new DateTimeZone('Etc/UCT'));
+    $errorCode = null;
+    $errorString = null;
+    $success = null;
+    $timeout = 30;
+
+    $start = microtime(true);
+    if ($fp = fsockopen($host, $port, $errorCode, $errorString, $timeout)) {
+        $success = true;
+        $errorCode = null;
+        $errorString = null;
+    } else {
+        $success = false;
+    }
+    fclose($fp);
+
+    $time = microtime(true) - $start;
+
+    return [
+        'date' => $date->format('Y-m-d H:i:s'),
+        'errorCode' => $errorCode,
+        'errorString' => $errorString,
+        'success' => $success,
+        'time' => $time,
+    ];
+}
+
+function connectToDb(string $host, string $user, string $pass, string $name)
+{
+    $dbh = new PDO("mysql:host={$host};dbname={$name}", $user, $pass);
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    return $dbh;
+}
+
+function logResult(PDO $db, array $result)
+{
+    $query = $db->prepare('INSERT INTO logs (date, success, time, errorCode, errorString) VALUES (?, ?, ?, ? ,?)');
+    $query->execute(array_values($result));
+}
+
+$db = connectToDb($config['dbHost'], $config['dbUser'], $config['dbPass'], $config['dbName']);
+
+$result = testConnection($config['testHost'], $config['testPort']);
+print_r($result);
+
+logResult($db);
---
+++ config.example.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'dbHost' => '127.0.0.1',
+    'dbName' => 'internet-checker',
+    'dbPass' => 'root',
+    'dbUser' => 'root',
+
+    'testHost' => 'www.google.com',
+    'testPort' => 443
+];
